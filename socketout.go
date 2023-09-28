361b2c8bff5f4ed8b454b6ed746348d771ef1408
ogios <2134692955@qq.com>
2023-09-28 17:32:13 +0800

    feat: add socketout;

--- socketout.go
+++ socketout.go
@@ -1,5 +1,34 @@
 package sutils
 
+import (
+	"io"
+	"sync"
+)
+
 type SBodyOUT struct {
-	Raw []byte
+	Raw   []any
+	Types []uint8
+	Cond  sync.Cond
+}
+
+func NewSBodyOUT() SBodyOUT {
+	return SBodyOUT{
+		Cond:  *sync.NewCond(&sync.Mutex{}),
+		Raw:   make([]any, 0),
+		Types: make([]uint8, 0),
+	}
+}
+
+func (so *SBodyOUT) AddBytes(raw []byte) {
+	so.Cond.L.Lock()
+	defer so.Cond.L.Unlock()
+	so.Raw = append(so.Raw, &raw)
+	so.Types = append(so.Types, 1)
+}
+
+func (so *SBodyOUT) AddReader(raw io.Reader) {
+	so.Cond.L.Lock()
+	defer so.Cond.L.Unlock()
+	so.Raw = append(so.Raw, raw)
+	so.Types = append(so.Types, 2)
 }
